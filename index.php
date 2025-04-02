d802ae5cac00521656206a92cf94760f86242810
Mattias Elmers <mattias.elmers@gmail.com>
2025-04-02 20:08:55 +0300

    f

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+<div>
+    <h1 class="text-center">PHP Harjutused</h1>
+        <ul>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h01.php">Harjutus 1</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h02.php">Harjutus 2</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h03.php">Harjutus 3</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h04.php">Harjutus 4</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h05.php">Harjutus 5</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h06.php">Harjutus 6</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h07.php">Harjutus 7</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h08.php">Harjutus 8</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h09.php">Harjutus 9</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h10/h10.php">Harjutus 10</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h12.php">Harjutus 12</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h13.php">Harjutus 13</a>
+            </li>
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="h14.php">Harjutus 14</a>
+            </li>
+        </ul>
+</div>
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
