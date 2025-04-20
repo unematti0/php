5837e633779ed44f80d62c1bbb696f6edc625fdf
Mattias Elmers <mattias.elmers@gmail.com>
2025-04-20 11:20:19 +0300

    htht

--- KT2/admin.php
+++ KT2/admin.php
@@ -9,7 +9,7 @@
   <body>
     <h1>admin leht!!!</h1>
     <h2>Lisa postitus:</h2>
-    <form>
+    <form method="POST">
 
         <div class="mb-3">
             <p>pealkiri:</p>
@@ -27,7 +27,6 @@
 
 <?php
 
-
  if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['lisa'])) {
     $pealkiri = $_POST['pealkiri'];
     $sisu = $_POST['sisu'];
@@ -35,15 +34,47 @@
     $minu_fail = fopen("postitused/$pealkiri.txt", 'w');
     fwrite($minu_fail, $sisu);
     fclose($minu_fail);
-    echo "<p>Postitus on lisatud!</p>";
+    echo "<p class='text-success'>Postitus on lisatud!!!</p>";
 }
-else {
-    echo "<p>tekksi viga</p>";
-}
+
 ?>
 
-<?php
+<h2>Kustusta postitus:</h2>
 
+
+
+    <form method="POST">
+        <div class="mb-3">
+            <p>pealkiri:</p>
+            <input type="text" class="form-control" id="pealkiri" name="pealkiri" required>
+        </div>
+
+        <button type="submit" name="kustuta" class="btn btn-danger">Kustuta postitus</button>
+    </form>
+<h3>olemas olevad postitused:</h3>
+<?php
+$postitused = glob('postitused/*.txt');
+
+foreach ($postitused as $postitus) {
+
+  $faili_info = pathinfo($postitus);
+  $pealkiri = $faili_info['filename'];
+
+  echo "$pealkiri<br>";
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['kustuta'])) {
+    $pealkiri = $_POST['pealkiri'];
+    $failinimi = "postitused/$pealkiri.txt";
+
+    if (file_exists($failinimi)) {
+        unlink($failinimi);
+        echo "<p class='text-success'>Postitus on kustutatud!!!</p>";
+    } else {
+        echo "<p class='text-danger'>Postitust ei leitud!!!</p>";
+    }
+}
 ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
